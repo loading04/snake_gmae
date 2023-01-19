0ebf46c59e7b11ec3a2311fa0dde8393afe3d88a
aziz <ahmed.aziz.smaoui@gmail.com>
2023-01-19 23:46:53 +0100

    highest score tracking feature

--- scoreboard.py
+++ scoreboard.py
@@ -8,6 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.penup()
         self.hideturtle()
         self.goto(0, 260)
@@ -15,13 +17,18 @@ class Scoreboard(Turtle):
         self.updateScore()
 
     def updateScore(self):
-        self.write(f"Score : {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score : {self.score} High Score : {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def increaseScore(self):
         self.score += 1
-        self.clear()
         self.updateScore()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("Game Over", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open(file="data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+
+        self.score = 0
+        self.updateScore()
--- snake.py
+++ snake.py
@@ -45,6 +45,13 @@ class Snake:
         # Add a new segment to the snake
         self.add_segment(self.segments[-1].position())
 
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
     def add_segment(self, position):
         new_segment = Turtle("square")
         new_segment.color("white")
